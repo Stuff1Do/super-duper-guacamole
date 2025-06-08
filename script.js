46262ef628016e6e39bb897da3fafa0773cadb3f
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2025-06-08 14:09:27 +0800

    add index style and script files, add basic html strucure, add styling, start script

---
+++ script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('.input-grid');
+const divContainer = document.querySelector('.div-container');
+
+
+let gridSize = 0;
+input.addEventListener('click', ()=>{
+    gridSize = prompt('Input grid size: (1 - 100):');
+})
+
+for(let i = 1; i <= gridSize; i++){
+    
+}
